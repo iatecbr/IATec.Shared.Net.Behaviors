6e13418b5971950c966c4a85399017197f357e5a
IATec - Gustavo Ferreira dos Santos <gustavo.fsantos@iatec.com>
2025-08-07 10:37:05 -0300

    feat: add exception pipeline behavior

---
+++ src/ExceptionPipelineBehavior.cs
@@ -0,0 +1,43 @@
+using FluentResults;
+using IATec.Shared.Behaviors.Resources;
+using MediatR;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace IATec.Shared.Behaviors;
+
+public class ExceptionPipelineBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : ResultBase, new()
+{
+    private readonly ILogger<ExceptionPipelineBehavior<TRequest, TResponse>> _logger;
+    private readonly IStringLocalizer<Messages> _localizer;
+
+    public ExceptionPipelineBehavior(
+        ILogger<ExceptionPipelineBehavior<TRequest, TResponse>> logger, IStringLocalizer<Messages> localizer)
+    {
+        _logger = logger;
+        _localizer = localizer;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, _localizer.GetString(nameof(Messages.InternalServerErrorMessage)));
+
+            var response = new TResponse();
+            response.Reasons.Add(new Error(_localizer.GetString(nameof(Messages.InternalServerErrorClientMessage))));
+
+            return response;
+        }
+    }
+}
